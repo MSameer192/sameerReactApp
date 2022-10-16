4db19b3d8f19639220e03e23b129fd0e496216ee
MSameer192 <sameer192.official@gmail.com>
2022-10-17 01:49:39 +0500

    new

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -13,6 +13,11 @@ export default function TextForm(props) {
     let newText = text.toLocaleLowerCase()
     setText(newText)
   }
+
+  const tOClearCase = () => {
+    let newText = ""
+    setText(newText)
+  }
   
   const tOHandleOnChange = (event) => {
     setText(event.target.value)
@@ -26,10 +31,13 @@ export default function TextForm(props) {
         <h1 className=''> {props.heading} </h1>
         <textarea className="form-control mt-4" id="exampleFormControlTextarea1" value={text} rows="10" onChange={tOHandleOnChange}></textarea>
         <button className="btn btn-primary mt-4 px-5" onClick={tOUpperCase} > TO UPPER CASE </button>
-        <button className="btn btn-primary mt-4 mx-5 px-5" onClick={tOLowerCase} > TO LOWER CASE </button>
+        <button className="btn btn-primary mt-4 mx-3 px-5" onClick={tOLowerCase} > TO LOWER CASE </button>
+        <button className="btn btn-primary mt-4 mx-3 px-5" onClick={tOClearCase} > TO CLEAR TEXT </button>
+
 
         <h2 className='mt-3'>Text Summary</h2>
         <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{ 0.008 * text.split(" ").length} Minutes read</p>
 
         <h2>Preview</h2>
         <p>{text}</p>
