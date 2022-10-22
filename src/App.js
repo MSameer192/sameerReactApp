b78214387bd270541e3d92269e65eed75d73c4f5
MSameer192 <sameer192.official@gmail.com>
2022-10-22 17:01:19 +0500

    login form

--- src/App.js
+++ src/App.js
@@ -1,50 +1,53 @@
 import './App.css';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+import Login from './components/Login';
+// import Navbar from './components/Navbar';
+// import TextForm from './components/TextForm';
 // import About from './components/About';
 
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
 function App() {
 
-  const [mode, setMode] = useState('light');
+  // const [mode, setMode] = useState('light');
 
-  const [modeBtn, setModeBtnText] = useState("Enable Dark Mode");
+  // const [modeBtn, setModeBtnText] = useState("Enable Dark Mode");
 
-  // const [myStyle1, myNewStyle] = useState({
-  //   color: "black",
-  // });
+  // // const [myStyle1, myNewStyle] = useState({
+  // //   color: "black",
+  // // });
 
-  const toggleMode = () => {
+  // const toggleMode = () => {
 
-    if (mode === "light") {
-      setMode('dark')
-      setModeBtnText('Disable Dark Mode')
-      document.body.style.backgroundColor = '#042743'
-      // myNewStyle({
-      //   color: 'white',
-      // })
-    }
+  //   if (mode === "light") {
+  //     setMode('dark')
+  //     setModeBtnText('Disable Dark Mode')
+  //     document.body.style.backgroundColor = '#042743'
+  //     // myNewStyle({
+  //     //   color: 'white',
+  //     // })
+  //   }
 
-    else {
-      setMode('light')
-      setModeBtnText('Enable Dark Mode')
-      document.body.style.backgroundColor = 'white'
-      // myNewStyle({
-      //   color: 'black'
-      // })
-    }
-  }
+  //   else {
+  //     setMode('light')
+  //     setModeBtnText('Enable Dark Mode')
+  //     document.body.style.backgroundColor = 'white'
+  //     // myNewStyle({
+  //     //   color: 'black'
+  //     // })
+  //   }
+  // }
 
 
 
   return (
     <div className="App">
       <>
-        <Navbar title="TexUtils" home="HOME" mode={mode} toggleMode={toggleMode} Btn={modeBtn} />
-        <TextForm heading="Put text for Change format" mode={mode} />
+        {/* <Navbar title="TexUtils" home="HOME" mode={mode} toggleMode={toggleMode} Btn={modeBtn} />
+        <TextForm heading="Put text for Change format" mode={mode} /> */}
 
         {/* <About/> */}
+
+        <Login/>
       </>
     </div>
   );
